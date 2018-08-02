8798b36188538c8cb934c4452793ca33874f30be
zhfanrui <15398280@uni.massey.ac.nz>
2018-08-02 15:43:42 +1200

    Create gogs.sh

---
+++ gogs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo Welcome!
+password=$1
+
+apt-get update -y
+apt-get upgrade -y
+
+apt-get install git postgresql-10 -y
+sudo -i -u postgres
+createuser gogs
+createdb gogs
+logout
+
+echo "$password
+$password
+
+
+
+
+
+Y
+" | sudo adduser gogs # need more instruction
+
+wget https://dl.gogs.io/0.11.53/gogs_0.11.53_linux_amd64.tar.gz
+
+tar -xvf gogs_0.11.53_linux_amd64.tar.gz
+
+cd gogs
+
+
+mkdir custom
+cd custom
+mkdir conf
+cd conf
+# app.ini
+local_ip=`curl ifconfig.me`
+echo $local_ip
+
+wget https://raw.githubusercontent.com/zhfanrui/CDN/master/app.ini
+mv app.ini app.ini.tmp
+sed "s/@username/$USER/g;s/@password/$password/g;s/@ip/$local_ip/g" app.ini.tmp > app.ini
+
+echo Success!
